a42dd9ab84b339d91baedafd0a72178bdcce02bb
Tsi777 <tsionphilipos6@gmail.com>
2025-07-20 04:47:15 -0700

    views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
 
-# Create your views here.
+# ViewSet for managing conversations
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        # Automatically associate the current user with the new conversation
+        conversation = serializer.save()
+        conversation.participants.add(self.request.user)  # Add the creator as a participant
+
+
+# ViewSet for managing messages
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        # Automatically set the sender to the current user
+        serializer.save(sender=self.request.user)
